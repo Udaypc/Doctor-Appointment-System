d221cc716caf3b4b64b873997a34bce337370edd
Uday Chauhan <chauhanuday4321@gmail.com>
2026-01-19 16:47:44 +0530

    add api gateway

--- patient-service/src/main/java/com/pateint_service/dto/PatientDto.java
+++ patient-service/src/main/java/com/pateint_service/dto/PatientDto.java
@@ -1,5 +1,9 @@
 package com.pateint_service.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +11,14 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PatientDto {
+    @NotNull
     private String name;
+    @NotNull
+    @Email(message = "Invalid Format")
     private String email;
+
+    @NotNull
+    @Size(min = 10,max = 10)
     private long contact;
     private String password;
 }
